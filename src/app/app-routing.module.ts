3808de56db14776a56d4a874bf78e9f3dfb2c85a
Jaspreet <jsprtmnn@gmail.com>
2020-04-25 23:26:24 +0530

    Created Login Design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DiscoverComponent } from './discover/discover.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SubscriptionComponent } from './subscription/subscription.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DiscoverComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'for-you', component: SubscriptionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import {
+  faGlobeAmericas,
+  faNewspaper,
+  faUserCircle,
+  faSearch,
+} from '@fortawesome/free-solid-svg-icons';
+import { DiscoverComponent } from './discover/discover.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SubscriptionComponent } from './subscription/subscription.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, DiscoverComponent, LoginComponent, ProfileComponent, SubscriptionComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(faGlobeAmericas, faNewspaper, faUserCircle, faSearch);
+  }
+}
