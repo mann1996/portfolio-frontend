7adf6ac85d9d7eb09f6d544cb431793ba0297aa2
Jaspreet Singh <jsprtmnn@gmail.com>
2020-08-03 11:24:11 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,16 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyDj6df8sNamgSDjxToKz5WCvNny6zmZ7-E',
-    authDomain: 'portfolio-mca2020.firebaseapp.com',
-    databaseURL: 'https://portfolio-mca2020.firebaseio.com',
-    projectId: 'portfolio-mca2020',
-    storageBucket: 'portfolio-mca2020.appspot.com',
-    messagingSenderId: '477024413890',
-    appId: '1:477024413890:web:6958dd239c415e1f34735b',
-    measurementId: 'G-0LS1BVC41L',
+    apiKey: ''
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
+    
+    // change above values according to your own server
   },
 };
 
