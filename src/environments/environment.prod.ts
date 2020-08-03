7f4e0b41b032b900975663b5fd7533a521b4fb87
Jaspreet Singh <jsprtmnn@gmail.com>
2020-08-03 11:22:04 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyDj6df8sNamgSDjxToKz5WCvNny6zmZ7-E',
-    authDomain: 'portfolio-mca2020.firebaseapp.com',
-    databaseURL: 'https://portfolio-mca2020.firebaseio.com',
-    projectId: 'portfolio-mca2020',
-    storageBucket: 'portfolio-mca2020.appspot.com',
-    messagingSenderId: '477024413890',
-    appId: '1:477024413890:web:6958dd239c415e1f34735b',
-    measurementId: 'G-0LS1BVC41L',
+    // Your firbase server config
   },
 };
